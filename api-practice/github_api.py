70deea4c98f18051b84c02e19e65237100e65da5
ISMAIL <ismailrisal@icloud.com>
2026-06-26 08:29:12 +0800

    feat: add Github profile fetch and parsing

--- api-practice/github_api.py
+++ api-practice/github_api.py
@@ -1,4 +1,4 @@
-import requests
+'''import requests
 
 def ambil_profil_github(username):
     try:
@@ -18,4 +18,29 @@ def ambil_profil_github(username):
     except requests.exceptions.ConnectionError:
         print("Tidak ada koneksi internet")
 
-ambil_profil_github("Ssmailoo")
+ambil_profil_github("Ssmailoo")'''
+
+import requests
+
+username = "Ssmailoo"
+
+url = f"https://api.github.com/users/{username}"
+
+response = requests.get(url)
+
+print(response.status_code)
+print(response.text)
+
+data = response.json()
+
+profile = {
+    "login": data['login'],
+    "company": data['company'] or "Tidak ditentukan",
+    "location": data['location'] or "Tidak ditentukan",
+    "html_url": data['html_url']
+}
+
+print("Username:", profile['login'])
+print("Company:", profile['company'])
+print("Lokasi:", profile['location'])
+print("GitHub:", profile['html_url'])
